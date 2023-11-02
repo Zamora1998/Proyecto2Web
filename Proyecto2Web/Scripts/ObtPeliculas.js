6ba0d9dd9d9d01ee93dd1bef9ac1e9b275126370
Johnny Zamora <JohnnyZ@IT-JOHNNY-PC>
2023-11-02 13:19:18 -0600

    Principal funcionando primera y segunda fila 5 pelis recientes por fecha

--- Proyecto2Web/Scripts/ObtPeliculas.js
+++ Proyecto2Web/Scripts/ObtPeliculas.js
@@ -21,26 +21,53 @@ function cargarPeliculas() {
             console.error(error);
         });
 }
+
 function mostrarPeliculas(peliculas) {
-    var container = document.getElementById("peliculas-container");
+    var containerPrimeraFila = document.getElementById("peliculas-container");
+    var containerSegundaFila = document.getElementById("peliculas-container-segunda-fila");
+
+    // Tamaño fijo para las imágenes de la primera fila
+    var imgWidthPrimeraFila = 800; // Ajusta el ancho según tus necesidades
+    var imgHeightPrimeraFila = 500; // Ajusta la altura según tus necesidades
 
     // Recorre las películas y crea elementos para mostrarlas
-    peliculas.forEach(function (pelicula) {
+    peliculas.forEach(function (pelicula, index) {
         var div = document.createElement("div");
         div.classList.add("col-md-4");
 
         var img = document.createElement("img");
         img.src = pelicula.rutaPoster;
-        img.classList.add("img-thumbnail"); // Agrega una clase para reducir el tamaño de la imagen
-        div.appendChild(img);
+        img.classList.add("img-thumbnail");
 
         var nombre = document.createElement("p");
         nombre.textContent = pelicula.nombre;
-        div.appendChild(nombre);
 
-        container.appendChild(div);
+        var resena = document.createElement("p");
+        resena.textContent = "Reseña: " + pelicula.resena;
+        resena.style.textAlign = "justify"; // Alinea el texto en justificado
+
+        var fechaEstreno = document.createElement("p");
+        fechaEstreno.textContent = "Fecha Estreno: " + new Date(pelicula.fechaLanzamiento).toLocaleDateString();
+
+        div.appendChild(img);
+        div.appendChild(nombre);
+        div.appendChild(resena);
+        div.appendChild(fechaEstreno);
+
+        if (index < 3) {
+            // Aplicar el tamaño fijo solo a las imágenes de la primera fila
+            img.style.width = imgWidthPrimeraFila + "px";
+            img.style.height = imgHeightPrimeraFila + "px";
+
+            containerPrimeraFila.appendChild(div);
+        } else {
+            containerSegundaFila.appendChild(div);
+        }
     });
 }
+
+
+
 
 // Cuando la página se carga, llama a la función para cargar las películas
 window.addEventListener("load", cargarPeliculas);
